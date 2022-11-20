d351b66686f9cfc45a7b3baaf92ae99c3b2437f9
jokersk <jokersk119@gmail.com>
2022-11-20 18:37:51 +0800

    wip

---
+++ src/input/input.vue
@@ -0,0 +1,12 @@
+<template>
+  <input
+    class="appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+<script setup lang="ts">
+defineProps({
+  modelValue: [String, Number]
+});
+</script>
