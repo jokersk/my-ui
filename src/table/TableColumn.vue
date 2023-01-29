e81c8e48d2a6d088b36461ecc91d53fe686c7d09
Szeto <jokersk119@gmail.com>
2023-01-29 11:56:24 +0800

    Update TableColumn.vue

--- src/table/TableColumn.vue
+++ src/table/TableColumn.vue
@@ -1,12 +1,8 @@
 <template></template>
 <script setup lang="ts">
-import { computed, inject, h, useSlots, onBeforeMount, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import { Options } from './Type'
+import { inject, h, useSlots, onMounted } from 'vue'
 import get from 'lodash/get'
-const router = useRouter()
-const addColumn = inject<Function>('addColumn')
-const getOptions = inject<() => Options>('getOptions')
+const addColumn = inject('addColumn')
 
 const props = defineProps<{
     prop?: string
@@ -15,7 +11,7 @@ const props = defineProps<{
     clickToDetail?: string
     /**
      * make the table th sortable
-     * example: <tableColumn sorting-key="name" prop="name" />
+     * example: 
      * now when user click the coresponse table th, the down arrow will appear
      * */
     sortingKey?: string
@@ -24,22 +20,9 @@ const { prop, label, clickToDetail, maxWidth = 'none' } = props
 
 const slot = useSlots()
 
-const normalizeLabel = () => {
-    if (typeof label === 'function') return label()
-    return label
-}
-
-const className =
-    'whitespace-nowrap px-3 py-4 text-sm cursor-pointer group-first:pl-6 truncate flex-grow'
-
 onMounted(() => {
-    const detailKey = getOptions()?.detailKey || 'id'
-    let onClick = getOptions()?.detailUrl
-        ? id => router.push({ name: getOptions().detailUrl, params: { id } })
-        : () => {}
     const attrs = row => ({
-        class: className,
-        onClick: () => onClick(get(row, detailKey)),
+        class: 'whitespace-nowrap px-3 py-4 text-sm cursor-pointer group-first:pl-6 truncate flex-grow',
         style: { 'max-width': maxWidth }
     })
     addColumn({
